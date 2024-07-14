5412aab875966fd34d2a8e837b2a7dbd8a963ef0
golphat1 <golphat@yahoo.com>
2024-07-14 22:27:30 +0300

    Implemented video upload feature

---
+++ mtusocial/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    //BEM naming convention
+    <div className="App">
+      <h1>Lets build a Full Stack MERN mtu social app</h1>
+    </div>
+  );
+}
+
+export default App;
